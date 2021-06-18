10b4c3c04728db7091ea9840e00da449d20ad64e
chrisshortlaw <chris@chrisshortlaw.com>
2021-06-18 13:45:28 +0100

    Refactored functions

--- assets/viewModel/vDom.js
+++ assets/viewModel/vDom.js
@@ -2,14 +2,14 @@
 // This file implements vDOM methods
 
 
-function h(tagName = 'div', attrs = {}, children = '0') {
+function h(tagName, attrs, props) {
     // h function will create vDOM elements which we shall manipulate
     // tagName: 'string', attrs: {}, children: ['String' OR '{tagName, attrs...}']
     // default parameters have been added to take out the tedium of creating objects
     return {
         tagName,
         attrs,
-        children
+        props
     }
 }
 
@@ -18,34 +18,35 @@ function mountVDOM(vnode, container) {
    // container is the part of the existing DOM which will hold the rendered VDOM.
    // create DOM element by using createElement built-in function
    const el = (vnode.el = document.createElement(vnode.tagName));
+
    // iterate over the js Object and set DOM attributes in accordance with the attrs assigned to the vDOM object
    for (const key in vnode.attrs) {
        el.setAttribute(key, vnode.attrs[key]);
    } 
 
-   // mount each of the children of the vDOM object
-   if (typeof vnode.children === 'string') {
-       el.textContent = vnode.children;
-   } else {
-       vnode.children.forEach((child) => {
-           mountVDOM(child, el); // uses recursion to mount the children
-       })
+   // loop over props object and assign data to textNode or attribute
+   for (const key in vnode.props) {
+       if (key == 'text') {
+           el.appendChild(document.createTextNode(vnode.props[key]));
+       } else {
+           el.setAttribute(key, vnode.props[key]);
+       }
    }
    // insert vnode in the actual DOM and render it.
-   container.appendChild(el)
+   container.appendChild(el);
 }
 
 function unmount(vnode) {
    vnode.el.parentNode.removeChild(vnode.el);
 }
 
-function patch(n1, n2) {
+/* function patch(n1, n2) {
    // compare two vnodes, identify differences
    // unsure what the below is doing when creating a constant
    const el = (n2.el = n1.el);
 
    // simple if state comparing the tags
-   if (n1.tag !== n2.tag) {
+   if (n1.tagName !== n2.tag) {
        mountVDOM(n2, el.parentNode);
        unmount(n1);
    } else {
@@ -82,6 +83,25 @@ function patch(n1, n2) {
            }
        }
    }
+} */
+
+function patchText(n1, n2) {
+    // takes two rendered objects with h
+    // assigns the el key/value of n2 to be the same as n1 - n2 may replace n1
+    const el = (n2.el = n1.el);
+    // if statement comparing each node
+    if (n2.tagName !== n1.tagName) {
+        mountVDOM(n2, el.parentNode);
+        unmount(n1);
+    } else {
+        if (n2.props['text'] !== n1.props['text']) {
+            el.removeChild(n1.props['text']);
+            el.appendChild(document.createTextNode(n2.props['text']));
+        } else {
+            continue;
+            // update this at a later stage
+        }
+    }
 }
 
-export {h, mountVDOM, unmount, patch};
+export {h, mountVDOM, unmount, patchText};
