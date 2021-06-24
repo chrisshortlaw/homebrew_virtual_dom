393d76aa90e22f2b0b08a120d02c8880f69364d7
chrisshortlaw <chris@chrisshortlaw.com>
2021-06-24 13:10:59 +0100

    Changed PUBSUB to instance PublishSubscribe cls

--- viewModel.js
+++ viewModel.js
@@ -1,4 +1,5 @@
 import { DATAMODEL } from './calc.js'; // imports functions from calc.js
+import {PublishSubscribe} from './pubsub.js' //
 // import * as vDom from './assets/viewModel/vDom.js';   // import vDOM functions
 
 
@@ -26,52 +27,9 @@ This viewModel will need to fulfil the following functions:
 The Publisher/Subscriber or Observer Pattern is a way to alert different functions, files or classes of a change in state in a certain function. The functions being alerted as subscribers; the function doing the alerting is the publisher. It is possible to have multiple publishers and multiple subscribers. Most front end frameworls operate a publish/subscribe pattern at some stage. In React, it is known as 'state'.
 */
 
-export const PUBSUB = {
-    // Topics object with hard-coded default topics
-    topics: {
-        validInput: [],
-        UserInput: [],
-        DataChange: [],
-        ClearInput: [],
-    },
+export const PUBSUB = new PublishSubscribe(['userInput', 'validInput', 'dataChange', 'clearInput']);
 
-    subscribe: function(message, callbackFunction) {
-        /**
-         * Sample Execution: PUBSUB.subscribe('UserInput', DATAMODEL.setUserInputString)
-         * Subscribe to UserInput channel
-         */
-        if (PUBSUB.topics.hasOwnProperty(message)) {
-             PUBSUB.topics.message.push(callbackFunction);
-            } else {
-                PUBSUB.topics.message = [callbackFunction];
-            }
-    },
-    publish: function(message, output) {
-        /**
-         * Sample: PUBSUB.publish('UserInput', '1')
-         * Publishes the string value one to 'UserInput' topic channel, value is an argument to each subscribing function.
-         */
-        if (PUBSUB.topics.hasOwnProperty(message)){
-            PUBSUB.topics.message.forEach((subscriber) => { subscriber(output) });
-        } else {
-            throw 'No Such Subscribed Topic';
-        }
-    },
-    get getSubscriberList(message) {
-        if (PUBSUB.topics.hasOwnProperty(message)) {
-            return PUBSUB.topics.message;
-        } else {
-            throw 'No Such Subscribed Topic';
-        }
-    },
-    get getAllSubscriptions() {
-        for (topic in topics) {
-            return [`${topic}:${topics[topic]}`];
-        }
-    }
-}
 
-    
 document.addEventListener('DOMContentLoaded', () => {
 
     const buttonList = Array.from(document.getElementsByTagName('button'));
@@ -99,12 +57,12 @@ function getUserInput(userInput) {
                 break;
             case 'clear':
                // DATAMODEL.clearDisplay;
-               PUBSUB.publish('ClearInput', '0');
+               PUBSUB.publish('clearInput', '0');
                // Change this to pub sub
                 break;
             default:
                 // DATAMODEL.num(clickInput); change this to pub sub
-                PUBSUB.publish('UserInput', userInput)        // publish change in state
+                PUBSUB.publish('userInput', userInput)        // publish change in state
                 break;
                 }
     } else {
