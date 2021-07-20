1c2eb63b2dc0b6b5d94f8f3610840d4e4d106d37
chrisshortlaw <chris@chrisshortlaw.com>
2021-07-20 10:38:59 +0100

    Added Comments. Updated References.

--- data/input_validator.js
+++ data/input_validator.js
@@ -1,9 +1,13 @@
-export const inputValidator = {
-    /* 
-    Validates inputs - no double operators, no multiple decimals etc.
-    Uses regular expressions to evaluate inputs
-    Use if/else or case statement before calling upon displayInput()
-    */
+import {PUBSUB} from '../viewModel.js'; // Change This Later
+
+/** 
+ * Validates inputs - no double operators, no multiple decimals etc. Uses regular expressions to evaluate inputs. 
+ * Use if/else or case statement before calling upon displayInput()
+*/
+
+export const inputValidatorRegex = {
+ 
+   
     decimal: new RegExp('(\d+\.\d+)|(\.\d+)'),
     notNumeral: new RegExp('\D|\W'),
     operator: new RegExp('\+|\-|\/|\*|\%'),
@@ -14,37 +18,66 @@ export const inputValidator = {
 
    
    // const arithExpInput = /(\d+)\s*([+*-/])\s*(\d+)/;
-    arithExpInput: new RegExp('^\d(?:\s[+*-/]\s\d)+$'),
-   
-    validate(input, currentString){ 
-        if (this.digitInput.test(input)) {
-            PUBSUB.publish('validInput', input)
-    } else if (this.decimalInput.test(input)) {
-            if (this.decimal.test(currentString)) {
+    arithExpInput: new RegExp('^\d(?:\s[+*-/]\s\d)+$')
+}
+    /**
+     * Tests input to see if valid string, either return validInput or calls an invalid input function to warn users  where multiple incorrect uses
+     * @param {string} input 
+     * @param {string} currentString 
+     * @returns String
+     */
+export function validate(input, currentString) { 
+    if (inputValidatorRegex.digitInput.test(input)) {
+        PUBSUB.publish('validInput', input)
+    } else if (inputValidatorRegex.decimalInput.test(input)) {
+            if (inputValidatorRegex.decimal.test(currentString)) {
                 return alert('Invalid Decimal Point Entry');
             } else {
                 return PUBSUB.publish('validInput', input)
             }
-    } else if (this.operatorInput.test(input)) {
+    } else if (inputValidatorRegex.operatorInput.test(input)) {
             let currentExp = currentString.slice(-1);
             if (operatorInput.test(currentExp)) {
-                return this.invalidInputCounter(this.invalidInputCount)
+                return (invalidInputCounter(invalidInputCount));
             } else {
                 return PUBSUB.publish('validInput', input);
             }
     } else {
-            return this.invalidInputCounter(this.invalidInputCount);
+            return invalidInputCounter(invalidInputCount);
         }
-    },
-    invalidInputCount: 0,
-    invalidInputCounter(inputCount){
+    }
+
+/**
+ * 
+ * @param {string} currentString Takes a string and checks it against a regular expression before publishing it
+ * @returns TRUE or publishes string to publishSubscribe CHANNEL
+ */
+export function validateEquation(currentString) {
+        if (inputValidatorRegex.arithExpInput.test(currentString)) {
+            /* FIX this to return TRUE */
+            return PUBSUB.publish('validEquation', currentString);
+        }
+    }
+    
+let invalidInputCount = 0;
+ /**
+  * 
+  * @param {Number} inputCount A number or a variable
+  * @returns Alert or increments number argument
+  */   
+function invalidInputCounter(inputCount){
+    if (typeof inputCount === 'Number') {  
         if (inputCount > 3){
+            inputCount = 0;
             return alert ('Multiple Invalid Inputs. Only numerals and arithmetic operators permitted. Double operators and decimal points are not permitted.');
         } else {
             return inputCount++;
         } 
+    } else {
+        return alert ('Invalid Argument: Must be a Number')
     }
 }
-
+//PUBSUB.subscribe('userInput', inputValidator.validate);
+//PUBSUB.subscribe('getEquation', inputValidator.validateEquation);
 
 
