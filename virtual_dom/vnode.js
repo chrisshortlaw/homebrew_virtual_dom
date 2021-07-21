45fce9e436fccb4683dccc7897e4a97650046e40
chrisshortlaw <chris@chrisshortlaw.com>
2021-07-21 14:52:56 +0100

    Renamed Builder Class. ++ Comments & Docstrings

--- virtual_dom/vnode.js
+++ virtual_dom/vnode.js
@@ -9,28 +9,50 @@ import {PropsBuild} from './props_build.js';
 */
 
 export class Vnode extends DomNode {
-    constructor(ComponentBuilder = {tagName, attrs, props}) {
-        super(ComponentBuilder.tagName);
-        //this.tagName = ComponentBuilder.tagName;
-        this.attrs = ComponentBuilder.attrs ?? {}; 
-        this.props = ComponentBuilder.props ?? {};
+    constructor(vnodeBuilder) {
+        super(vnodeBuilder.tagName);
+        //this.tagName = vnodeBuilder.tagName;
+        this.attrs = vnodeBuilder.attrs ?? {}; 
+        this.props = vnodeBuilder.props ?? {};
 
-        if (ComponentBuilder != null) {
-            if (ComponentBuilder.props?.children != undefined && ComponentBuilder.props?.children instanceof Array) {
-                ComponentBuilder.props.children.forEach(child => this.add(child));
+        if (vnodeBuilder.props?.children != null) {
+            if (vnodeBuilder.props?.children != undefined && vnodeBuilder.props?.children instanceof Array) {
+                vnodeBuilder.props.children.forEach(child => this.add(child));
             } else {
                 // Put Something Here?!?
             }
         }
     }
-    static get ComponentBuilder() {
-        class ComponentBuilder {
-            constructor(tagName){
-                this.tagName = tagName;
-                
+    static get vnodeBuilder() {
+        class vnodeBuilder {
+            /**
+             * This builds our vNode for us and helps us to write objects in a fluent style. Note, the vNodeBuilder parameter is optional.
+             * Note: If using fluent style (vnode.Builder.withTagName())
+             * @param {string} tagName Tag for HTML, defaults to 'div' if none supplied. HTML. The virtual DOM does not check this for validity,
+             * @param {object} vnodeBuilder Object comprised of attrs and props. Optional as both default to empty object literals if nothing is passed. 
+             */
+            constructor(tagName, vnodeBuilder){
+                this.tagName = tagName || (vnodeBuilder?.tagname ?? 'div');
+                this.attrs = vnodeBuilder?.attrs ?? {};
+                this.props = vnodeBuilder?.props ?? {};
+            
             }
+            /**
+             * 
+             * @param {string} tag Tag for HTML, defaults to 'div' if none supplied. HTML. The virtual DOM does not check this for validity,
+             * @returns Object with {'tagName': {your_tag_here}}
+             */
+            withTagName(tag){
+                this.tagName = tag;
+                return this;
+            }
+            /**
+             * Returns an attrs object to the instance. This function can be passed an AttrsBuild instance (including AttrsBuild.AttrsBuilder()).
+             * @param {Object} attrs Object Comprised of attrs
+             * @returns 
+             */
             withAttrs(attrs) {
-                this.attrs = new AttrsBuild(new AttrsBuild.AttrsBuilder);
+                this.attrs = attrs;
                 return this;
                  
             }
@@ -40,9 +62,70 @@ export class Vnode extends DomNode {
                 
             }
             build() {
-                return new ComponentBuilder(this);
+                return new vnodeBuilder(this.tagName, this);
             }
         }
-        return ComponentBuilder;
+        return vnodeBuilder;
+    }
+    /**
+     * This adds a node as a child to the vNode. Also sets the CompID and ParentID on the node.
+     * 
+     * 
+     * @param {any} node Must be instanceof Vnode or string;
+     */
+    add(node) {
+        if (node instanceof Vnode || typeof node === 'string') {
+            if ((this.CompID != null || 0) || node instanceof Vnode) {
+                const CompNum = (this._CompID * 100) + this.children.length;
+                node.CompID = CompNum;
+                node.ParentId = this.CompID;
+            }
+            this._children.push(node);
+            
+        } else {
+            throw new TypeError('Node must be string or Vnode');
+        }
+    }
+    get children(){
+        return this._children;
+    }
+    /**
+     * 
+     * @param {integer} index index number of child node in array.
+     * @returns A string or a Vnode instance
+     */
+    getChild(index) {
+        return this._children[index];
+    }
+    /**
+     * Checks to see if node has any child nodes
+     * @returns true or false
+     */
+    hasChildren() {
+        if ((typeof index == 'number'|| 'bigInt') && index != NaN) {
+            if (index < this._children.length) {
+                return this._children[index];
+            } else {
+                throw('DomTree.getChild(): Argument exceeds index');
+            }
+            
+        } else {
+            throw('DomTree.getChild() only takes number as parameter');
+        }
+    }
+    set ParentID(id) {
+        this._ParentID = id;
+
+    }
+    set CompID(id) {
+        this.CompID = id;
+        let i = 1;
+        this.children.forEach(child => {
+            if ((child instanceof vnode || DomNode) && child.ParentID == null && child.CompID == null) {
+                child.ParentID = this.CompID;
+                child.CompID = (this.CompID * 100) + i;  
+            }
+            i++; 
+        })
     }
 }
